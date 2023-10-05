9e56ebebbefe4ad3db91752b80392591f8840f80
Chloeangggg <96276634+Chloeangggg@users.noreply.github.com>
2023-10-05 18:46:41 +1100

    Add files via upload

---
+++ js/vega_lite_vis.js
@@ -0,0 +1,14 @@
+var spec2 = "js/subwaymap.vg.json";
+vegaEmbed('#subwaymap', spec2).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var spec2 = "js_nut/category.vg.json";
+vegaEmbed('#category', spec2).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var spec3 = "js_nut/sugar.vg.json";
+vegaEmbed('#sugars', spec3).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
